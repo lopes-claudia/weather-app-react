60ae0d1b4d7a46cbf100b7b610f412ebbb60e17c
lopes-claudia <62217035+lopes-claudia@users.noreply.github.com>
2020-06-28 17:15:16 +0100

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Loader from "react-loader-spinner";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Loader type="Rings" color="#008080" height={70} width={70} />
       </header>
     </div>
   );
 }
-
-export default App;
